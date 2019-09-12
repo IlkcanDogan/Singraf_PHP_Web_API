81a5dbca7165f93c5bb17983ff22abf63bdf5169
İlkcan Doğan <40579186+IlkcanDogan@users.noreply.github.com>
2019-09-12 18:43:26 +0300

    Update onay.php

--- Server 1/onay.php
+++ Server 1/onay.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="Singraf">
-  <meta name="author" content="İlkcan Doğan & Ali Ergüç">
+  <meta name="author" content="İlkcan Doğan">
 
   <title>Singraf Hesap Onayı</title>
   <link rel="icon" type="image/png" href="web/img/slogo2.png"/>
